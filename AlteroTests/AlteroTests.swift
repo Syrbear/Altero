995a9113869b2c6342bb9e00f2d57f272e0be85f
Syrbear <geunwoong.sim@gmail.com>
2026-03-27 15:18:27 +0900

    init proj and ignore

---
+++ AlteroTests/AlteroTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlteroTests.swift
+//  AlteroTests
+//
+//  Created by 심근웅 on 3/27/26.
+//
+
+import Testing
+@testable import Altero
+
+struct AlteroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
